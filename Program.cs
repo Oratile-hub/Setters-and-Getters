94df215b5f651ece066cfe77b9d1cda435963b8b
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-14 14:21:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setters_and_Getters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Oratile", 28, "Male");
+            Student student2 = new Student("Jim", 25, "K");
+
+            Console.WriteLine(student1.name);
+            Console.WriteLine(student1.gender);
+            Console.WriteLine(student2.name);
+            Console.WriteLine(student2.gender);
+            Console.WriteLine(Student.Studentcount);
+            
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Setters_and_Getters
+{
+    internal class Student
+    {
+        public string name;
+        public int age;
+        public string gender;
+
+        public static int Studentcount = 0;
+
+        //constructor
+        public Student(string aName, int aAge, string aGender)
+        {
+            name = aName;
+            age = aAge;
+            Gender = aGender;
+            Studentcount += 1;
+
+        }
+        public string Gender 
+        { 
+            get 
+            { return gender; }
+
+            set
+            {
+                if (value == "Male" || value == "Female")
+                { gender = value; }
+                else { gender = "Prefer not to say"; }
+
+            }
+        }
+        
+    }
+}
+
